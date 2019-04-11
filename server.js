9e5212fa8c20dc7312183d8a4758aac70abd6b34
Emilykradke <emilykradke@gmail.com>
2019-04-11 17:53:35 -0500

    update html

--- server.js
+++ server.js
@@ -32,7 +32,31 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 // Routes:
 
 // A GET route for scraping the cinemablend website and then add it to the db
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.cinemablend.com/news.php").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
+    // Now, we grab every div with a 'story_item'class
+    $("div.story_item").each(function(i, element) {
+      var result = {};
+
+      // Add all of the elements and save them as properties
+      result.image = $(element).find("img").attr("src");
+      result.author = 
+      result.title = $(element).find("a").attr("title");
+      result.link = $(element).find("a").attr("href");
+      result.summary = $(element).find(".story_summary.p").text();
+      // result.date = 
+ 
+      console.log(result)
+    })
+
+  })
+    
+     
+})
 
 // A GET route for GETting all of the articles from the db
 
