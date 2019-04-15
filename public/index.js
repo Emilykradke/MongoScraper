793e3e2ec8814b6229e192f9298939d624865081
Emilykradke <emilykradke@gmail.com>
2019-04-14 21:38:57 -0500

    finished article divs

--- public/index.js
+++ public/index.js
@@ -2,21 +2,24 @@
 $(document).on("click", "#scrapeArticlesButton", function() {
   $(".articles").empty();
 
+  const articleDiv = document.querySelector(".articles");
+  articleDiv.scrollIntoView({ block: "start", behavior: "smooth"});
+
   $.getJSON("/scrape", function(req, res) {
   })
 
   $.getJSON("/articles", function(data) {
     console.log(data);
-    // ("#articleCard").empty();
-
     
     for (var i = 0; i < 20; i ++) {
       // Display the information on the page
-      // $("#articleCard").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].image + "</p>")
       let articleDiv = $("<div>").append(
-        $("<img>").attr("src", data[i].image),
-        $("<p>").text(data[i].title).addClass("article-title").attr("href", data[i].link),
-        $("<p>").text(data[i].summary).addClass("article-summary")
+        $("<img>").attr("src", data[i].image).attr("href", "https://www.empireonline.com" + data[i].link).addClass("card-image"),
+        $("<div>").addClass("infoContainer").append(
+          $("<a>").text(data[i].title).addClass("article-title").attr("href", "https://www.empireonline.com" + data[i].link),
+          $("<p>").text(data[i].summary).addClass("article-summary")
+        ),
+        $("<button>").text('Save Article').addClass('btn-save').attr('type', 'submit').attr('data-id', data[i]._id)
       ).addClass("card").attr("data-id", data[i]._id)
       $(".articles").append(articleDiv)
     }
